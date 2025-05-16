c78a4fe33466e616bfc68ecdc9416c154ce4090c
Eniwder <akabudou0120@yahoo.co.jp>
2025-05-16 22:39:44 +0900

    delete comment

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -21,12 +21,6 @@
           </v-btn>
         </v-col>
       </v-row>
-      <!-- <v-row no-gutters style="height: 190px">
-        <v-col>
-          <v-data-table :headers="histories.headers" :items="histories.body" :items-per-page="3" hide-default-footer
-            class="elevation-1"></v-data-table>
-        </v-col>
-      </v-row> -->
       <v-row no-gutters>
         <v-col class="echart-wrapper">
           <VChart class="chart" :option="option" autoresize ref="chart" />
@@ -94,22 +88,6 @@ const option = reactive({
 const inputIpport = ref('');
 const chartRef = ref(null);
 const checkbtn = reactive({ progress: false });
-// const histories = reactive({
-//   headers: [
-//     { text: 'target', align: 'start', sortable: false, value: 'ipport' },
-//     { text: '1', value: '1' },
-//     { text: '2', value: '2' },
-//     { text: '3', value: '3' },
-//     { text: '4', value: '4' },
-//     { text: '5', value: '5' },
-//     { text: '6', value: '6' },
-//     { text: '7', value: '7' },
-//     { text: '8', value: '8' },
-//     { text: '9', value: '9' },
-//     { text: 'avg', value: 'avg' },
-//   ],
-//   body: [],
-// });
 
 const ipportRule = computed(() => {
   const pattern =
@@ -138,25 +116,6 @@ function ping(ipport) {
       window.ipcRenderer.removeAllListeners('result-ping');
       const result = option.series[0].data;
       checkbtn.progress = false;
-      // なんかヘッダーが表示されないしいらない気もしてきたから一旦消す
-      // histories.body.unshift({
-      //   ipport: ipport,
-      //   1: ('' + (result[0] || 100)).substring(0, 3).padEnd(2, 0),
-      //   2: ('' + (result[1] || 100)).substring(0, 3).padEnd(2, 0),
-      //   3: ('' + (result[2] || 100)).substring(0, 3).padEnd(2, 0),
-      //   4: ('' + (result[3] || 100)).substring(0, 3).padEnd(2, 0),
-      //   5: ('' + (result[4] || 100)).substring(0, 3).padEnd(2, 0),
-      //   6: ('' + (result[5] || 100)).substring(0, 3).padEnd(2, 0),
-      //   7: ('' + (result[6] || 100)).substring(0, 3).padEnd(2, 0),
-      //   8: ('' + (result[7] || 100)).substring(0, 3).padEnd(2, 0),
-      //   9: ('' + (result[8] || 100)).substring(0, 3).padEnd(2, 0),
-      //   avg:
-      //     (
-      //       result.reduce((acc, v) => acc + (v || 100), 0) / result.length
-      //     )
-      //       .toFixed(2)
-      //       .padEnd(2, 0) + ' F',
-      // });
     } else {
       option.series[0].data.push(parseFloat(arg) || 100);
       const chart = chartRef.value?.chartInstance;
